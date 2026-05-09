ec31ddfbf0e7d356acae6462972664d30b2944fa
Ruslan <RuslanFedorov303@gmail.com>
2026-05-09 17:33:14 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,22 @@ from customtkinter import *
 from PIL import Image #pillow
 
 
+import sys
+import os
+
+
+def resource_path(relative_path):
+    """ Отримує абсолютний шлях до ресурсів, працює для dev та для PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
 class AuthWindow(CTk):
     def __init__(self):
         super().__init__()
